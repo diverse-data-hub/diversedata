7b051818f9832ec859f417b48ccb12f05e0c24a3
AzinPiran <az.p.ctc@gmail.com>
2025-05-21 12:59:15 -0700

    Wildfire data added to the package

--- R/data-documentation.R
+++ R/data-documentation.R
@@ -4,3 +4,12 @@
 #' @format A data frame of Indigenous-owned businesses in BC
 #' @source Government of BC
 "bcindigenousbiz"
+
+#' Alberta Historical Wildfire Dataset
+#'
+#' @format This dataset contains information about historical wildfire incidents in Alberta.
+#' 
+#'
+#' @source \url{https://open.alberta.ca/licence}
+#' Data retrieved from Alberta Open Data under the Alberta Open Government Licence.
+"wildfire"
--- data-raw/prepare-data.R
+++ data-raw/prepare-data.R
@@ -5,9 +5,12 @@ library(usethis)
 
 # Load each dataset
 bcindigenousbiz <- read_csv("data-raw/cleaned_indigenous_businesses.csv")
-
+wildfire <- read_csv("data-raw/wildfire.csv")
 # Save all datasets into the package
 use_data(
   bcindigenousbiz,
+  wildfire,
   overwrite = TRUE
 )
+
+
