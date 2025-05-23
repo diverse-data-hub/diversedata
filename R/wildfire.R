1511d6082af5af85cd216cf86717817bf61245d8
AzinPiran <az.p.ctc@gmail.com>
2025-05-23 16:22:58 -0700

    Wildfire dataset added and documented

---
+++ R/wildfire.R
@@ -0,0 +1,8 @@
+#' Alberta Historical Wildfire Dataset
+#'
+#' This dataset contains information about historical wildfire incidents in Alberta.
+#'
+#' @format A data frame contains information about historical wildfire incidents in Alberta.
+#' @source \url{https://open.alberta.ca/licence}
+#' Data retrieved from Alberta Open Data under the Alberta Open Government Licence.
+"wildfire"
