2c608b9e175019089060a65c59a17219ad91935b
AzinPiran <az.p.ctc@gmail.com>
2025-05-26 11:18:24 -0700

    Store global rights dataset in the data-clean folder

--- data-clean/prepare-data.R
+++ data-clean/prepare-data.R
@@ -4,10 +4,14 @@ library(readr)
 library(usethis)
 
 # Load each dataset
-bcindigenousbiz <- read_csv("data-clean/cleaned_indigenous_businesses.csv")
+bcindigenousbiz <- read_csv("data-raw/cleaned_indigenous_businesses.csv")
+wildfire <- read_csv("data-raw/wildfire.csv")
+globalrights <- read_csv("data-raw/globalrights.csv")
 
 # Save all datasets into the package
 use_data(
   bcindigenousbiz,
+  wildfire,
+  globalrights,
   overwrite = TRUE
 )
