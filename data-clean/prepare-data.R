3bb8276caa981dc0b631d9c32f6e8f94c4fcd3d5
AzinPiran <az.p.ctc@gmail.com>
2025-05-27 10:43:47 -0700

    Hcmst and womensmarchmadness datasets added

--- data-clean/prepare-data.R
+++ data-clean/prepare-data.R
@@ -7,11 +7,16 @@ library(usethis)
 bcindigenousbiz <- read_csv("data-clean/cleaned_indigenous_businesses.csv")
 wildfire <- read_csv("data-clean/wildfire.csv")
 globalrights <- read_csv("data-clean/globalrights.csv")
+hcmst <- read_csv("data-clean/hcmst.csv")
+womensmarchmadness <- read_csv("data-clean/womensmarchmadness.csv")
+
 
 # Save all datasets into the package
 use_data(
   bcindigenousbiz,
   wildfire,
   globalrights,
+  hcmst,
+  womensmarchmadness,
   overwrite = TRUE
 )
